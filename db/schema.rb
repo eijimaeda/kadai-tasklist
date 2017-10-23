025ba9372e4fed9c8a42ea2d727ddd81232a5018
eijimaeda <eiji.maeda0611@gmail.com>
2017-10-23 04:23:36 +0000

    CRUD tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171020053226) do
+ActiveRecord::Schema.define(version: 20171021222930) do
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date     "deadline"
   end
 
 end
