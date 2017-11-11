499d739fa6caff34d981c9dce7dd968362c33edb
eijimaeda <eiji.maeda0611@gmail.com>
2017-11-11 12:59:00 +0000

    kadai-tasklist-login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,26 +1,29 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
   before_action :set_task, only: [:show, :edit, :update, :destroy ]
   
-def index
-  @tasks = Task.all.page(params[:page])
-end
+#def index
+#  @tasks = Task.all.page(params[:page])
+#end
 
   def show
     set_task
   end
 
   def new
-    @task = Task.new
+    @task = current_user.tasks.build
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:success] = 'Taskが正常に作成されました'
-      redirect_to @task
+      redirect_to root_url
     else
+      @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
       flash.now[:danger] = 'Taskが作成されませんでした'
-      render :new
+      render 'toppages/index'
     end
   end
 
@@ -40,17 +43,22 @@ end
   end
 
   def destroy
-    set_task
     @task.destroy
-    
     flash[:success] = 'taskは正常に削除されました'
-    redirect_to tasks_url
+    redirect_to root_path
   end
   
   private
   
   def set_task
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.find_by(id: params[:id])
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
   
   def task_params
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171108135138) do
+ActiveRecord::Schema.define(version: 20171110101920) do
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20171108135138) do
     t.datetime "updated_at", null: false
     t.date     "deadline"
     t.string   "status"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
